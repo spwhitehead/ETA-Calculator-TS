e78b8be747b01d18c9a40e50b0cf41eb8bbb3666
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-07-16 12:33:18 -0600

    Fix formatting

--- src/Components/Destination/Destination.tsx
+++ src/Components/Destination/Destination.tsx
@@ -4,7 +4,7 @@ export default function Destination() {
     return (
         <main className={styles.container}>
             <label htmlFor="destination-input">Enter Estimated Number of Miles:</label>
-            <input type="number" id="destination-input" className={styles.input} />
+            <input type="number" id="destination-input" placeholder="Number of Miles" className={styles.input} />
         </main>
     );
 }
--- src/Components/Speed/Speed.tsx
+++ src/Components/Speed/Speed.tsx
@@ -4,7 +4,7 @@ export default function Speed() {
     return (
         <main className={styles.container}>
             <label htmlFor="speed-input">Enter Your Estimated Speed (mph):</label>
-            <input type="number" id="speed-input" className={styles.input} />
+            <input type="number" id="speed-input" placeholder="Speed in MPH" className={styles.input} />
         </main>
     );
 }
